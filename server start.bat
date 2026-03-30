2fbab8582eac5414cfd1fc62e3417a182c9ab6bd
NJEPOh <hafidzalifr8899@gmail.com>
2026-03-30 15:41:19 +0700

    Backup 15-41, 30-03-2026

--- server start.bat
+++ server start.bat
@@ -13,4 +13,31 @@ echo Starting Minecraft Server...
 java -Xmx6G -Xms6G -XX:+UseG1GC -jar server.jar nogui
 
 echo Server stopped.
+
+:: Ambil tanggal & jam
+for /f "tokens=1-4 delims=/ " %%a in ("%date%") do (
+    set day=%%a
+    set month=%%b
+    set year=%%c
+)
+
+for /f "tokens=1-2 delims=: " %%a in ("%time%") do (
+    set hour=%%a
+    set minute=%%b
+)
+
+:: Format commit message
+set msg=Backup %hour%-%minute%, %day%-%month%-%year%
+
+echo ==========================
+echo COMMIT MESSAGE: %msg%
+echo ==========================
+
+:: Git commands
+git add .
+git commit -m "%msg%"
+echo "Pushing code to GitHub..."
+git push origin main --force
+echo "Success"
+
 pause
